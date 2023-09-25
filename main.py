70e9a1eaf92117864f6bb3cebc2e2637a4aebf9d
LakshmiYerragudi <lakshmi@wetrancsloud.com>
2023-09-25 09:12:48 +0000

    latest

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ def hello_world():
     return 'THIS IS SAMPLE FLASK APPLICATION.'
  
 if __name__ == "__main__":
-   port = int(os.environ.get('PORT', 5000))
+   port = int(os.environ.get('PORT', 8080))
    app.run(debug=True, host='0.0.0.0', port=port)
